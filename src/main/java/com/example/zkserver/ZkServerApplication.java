fc797921a9b0febc1fe27d099221a0cf97d59bdb
MarekDudek <marek.dudek@gmail.com>
2023-08-22 00:27:45 +0200

    zk server

---
+++ src/main/java/com/example/zkserver/ZkServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.zkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkServerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ZkServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/zkserver/ZkServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.zkserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkServerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
